5065bd685af36c1a9100cc88d5c8d8beff6474c8
mfeingold@hill30.com <mfeingold@hill30.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2010-06-23 22:57:23 +0000

    git-svn-id: https://ndjango.googlecode.com/svn/NDjango/trunk@523 5ca2c0c4-756c-11de-8369-6b41a2a8383f

---
+++ NDjangoDesigner/CodeCompletion/CompletionSets/ReferenceCompletionSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Language.Intellisense;
+
+namespace NDjango.Designer.CodeCompletion.CompletionSets
+{
+    class ReferenceCompletionSet : AbstractCompletionSet
+    {
+        public ReferenceCompletionSet() { }
+
+        protected override IEnumerable<Completion> BuildNodeCompletions()
+        {
+            var facets = getPrefix().Split('.');
+            return BuildCompletions(
+                buildMemberList(facets.ToList(), Node.Context.Variables).Select(var => var.Name),
+                Prefix, Suffix);
+        }
+
+        private IEnumerable<Interfaces.IDjangoType> buildMemberList(List<string> facets, IEnumerable<Interfaces.IDjangoType> members)
+        {
+            var instance = Node.Context.Variables.First(member => member.Name == facets[0]);
+            if (instance == null)
+                return null;
+            facets.RemoveAt(0);
+            if (facets.Count == 0)
+                return instance.Members;
+            return buildMemberList(facets, instance.Members);
+        }
+
+        protected virtual string Prefix { get { return ""; } }
+
+        protected virtual string Suffix { get { return ""; } }
+    }
+}
