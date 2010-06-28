3030874dedb37849d77bd69da3557e69e7a74c5f
mfeingold@hill30.com <mfeingold@hill30.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2010-06-28 15:06:24 +0000

    Designer type resolver
    git-svn-id: https://ndjango.googlecode.com/svn/NDjango/trunk@531 5ca2c0c4-756c-11de-8369-6b41a2a8383f

---
+++ NDjangoDesigner/Parsing/TypeResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+using System.ComponentModel.Design;
+
+namespace NDjango.Designer.Parsing
+{
+    public class TypeResolver : NDjango.TypeResolver.ITypeResolver
+    {
+
+        private SVsServiceProvider serviceProvider;
+
+        private ITypeResolutionService type_resolver;
+
+        [Import]
+        internal SVsServiceProvider ServiceProvider
+        {
+            get { return serviceProvider; }
+            private set
+            {
+                serviceProvider = value;
+                type_resolver = GetService<ITypeResolutionService>();
+            }
+        }
+
+        private T GetService<T>()
+        {
+            return (T)ServiceProvider.GetService(typeof(T));
+        }
+
+        private T GetService<T>(Type serviceType)
+        {
+            return (T)ServiceProvider.GetService(serviceType);
+        }
+
+
+        #region ITypeResolver Members
+
+        public IEnumerable<Interfaces.IDjangoType> Resolve(string type_name)
+        {
+            var type = type_resolver.GetType(type_name);
+            foreach (var member in type.GetMembers())
+                yield return new NDjango.TypeResolver.ValueType(member.Name);
+        }
+
+        #endregion
+    }
+}
