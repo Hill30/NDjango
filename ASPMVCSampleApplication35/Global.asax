16f4572c83a766d62f995301d14c2269154ac344
kirillskrygan@gmail.com <kirillskrygan@gmail.com@5ca2c0c4-756c-11de-8369-6b41a2a8383f>
2010-08-04 15:52:24 +0000

    Sample Application for .NET 3.5
    git-svn-id: https://ndjango.googlecode.com/svn/NDjango/trunk@615 5ca2c0c4-756c-11de-8369-6b41a2a8383f

---
+++ ASPMVCSampleApplication35/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPMVCSampleApplication40.MvcApplication" Language="C#" %>
---
+++ ASPMVCSampleApplication35/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ASPMVCSampleApplication35")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ASPMVCSampleApplication35")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("008e1b30-d4e5-46a9-8777-58d12bb81ff9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
