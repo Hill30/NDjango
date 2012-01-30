c135d8d417ba3609e5578623d0e10346cd15464e
sivanov-hill30 <sivanov@hill30.com>
2012-01-30 17:05:03 +0400

    Issue #11 - added missing class

---
+++ NDjangoDesigner/Misc/otherStuff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NDjango.Designer.Misc
+{
+    /// <summary>
+    /// Class containing static helper methods
+    /// </summary>
+    public static class otherStuff
+    {
+        public static void ShowErrorDialog(string caption, string msg) 
+        {
+            MessageBox.Show(
+                msg,
+                caption,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+    }
+}
